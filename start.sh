44fdd1d14005437ee0b27e1fda3a96a90d56fe75
Cory Borek <agentdid127cb@aol.com>
2025-04-18 12:17:40 -0400

    t

--- start.sh
+++ start.sh
@@ -5,6 +5,6 @@ export NVM_DIR="$HOME/.nvm"
 [ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"
 [ -s "$NVM_DIR/bash_completion" ] && \. "$NVM_DIR/bash_completion"
 
-cd /home/user/message_flagger
+cd /home/user/Radio-Margaritaville/
 export NODE_ENV=development
 npm start
