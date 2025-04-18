25cd71918447198face7964ced6805e97b9195f4
Cory Borek <agentdid127cb@aol.com>
2025-04-18 12:17:12 -0400

    add start.sh

---
+++ start.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+source ${HOME}/.bashrc
+
+export NVM_DIR="$HOME/.nvm"
+[ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"
+[ -s "$NVM_DIR/bash_completion" ] && \. "$NVM_DIR/bash_completion"
+
+cd /home/user/message_flagger
+export NODE_ENV=development
+npm start
